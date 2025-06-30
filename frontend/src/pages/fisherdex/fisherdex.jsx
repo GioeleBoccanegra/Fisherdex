78ce9053f059039193436cba81fe9b7a5ca603be
giobocca03 <gio.bocca03@gmail.com>
2025-06-30 14:36:58 +0200

    abbozzato primo stile your  fisherdex

--- frontend/src/pages/fisherdex/fisherdex.jsx
+++ frontend/src/pages/fisherdex/fisherdex.jsx
@@ -1,13 +1,28 @@
 import Fishcard from "./fishcard/fishcard";
+import { useState } from "react";
+import "./fisherdex.css"
 
 export default function Fisherdex() {
+  const [checked, setChecked] = useState(false);
   return (
     <div>
-      <h1>Fisherdex</h1>
-      <div className="fisherdex-container">
+      <h1>Your Fisherdex</h1>
       
-        <Fishcard />
+      <div className="fisherdex-container">
+      <div className="filters">
+        <div className="captured-fish">
+          <a>captured</a>
+          <input
+        type="checkbox"
+        checked={checked}
+        onChange={() => setChecked(!checked)}
+      />
+        </div>
       </div>
+      <Fishcard />
+    </div>
+     
+      
     </div>
   );
 }
