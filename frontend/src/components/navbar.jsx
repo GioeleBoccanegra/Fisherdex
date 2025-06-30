5a5fe77325f5e4f9ed5160e1e731fc813d5feb48
giobocca03 <gio.bocca03@gmail.com>
2025-06-30 14:50:35 +0200

    sistemata estetica navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
-
+import logo from "../assets/logo-fisherdex.jpeg";
+import "./navbar.css";
 
 export default function Navbar() {
   return (
     <div className="navbar">
+      <img src={logo} alt="logo" className="logo"></img>
        <Link to="/user">User</Link>
       <Link to="/">Home</Link>
       <Link to="/mappa">Mappa</Link>
