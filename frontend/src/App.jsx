98a15d4b950d10f2f832f288c684502f9ee4485e
giobocca03 <gio.bocca03@gmail.com>
2025-07-04 14:35:39 +0200

    creato modello specie

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import Register from './pages/register/register'
 function App() {
 
   const[isAuthenticated, setIsAuthenticated] = useState(false);
+  const [checkingAuth, setCheckingAuth] = useState(true);
 
 
   //controllo se loggato
@@ -19,7 +20,17 @@ function App() {
   useEffect(()=>{
     const token=localStorage.getItem('token');
     setIsAuthenticated(!!token);
+    setCheckingAuth(false);
   },[])
+
+  //Finché checkingAuth è true, il componente App non renderizza le rotte e non fa redirect, ma mostra solo un placeholder (ad esempio: "Caricamento..." o uno spinner).
+
+  /*React aspetta di sapere se l’utente è autenticato o no prima di decidere quale pagina mostrare.
+Solo dopo aver controllato il token (quindi quando checkingAuth diventa false) viene renderizzato il router e le rotte, che leggono lo stato di autenticazione vero e definitivo.*/
+
+  if (checkingAuth) {
+    return <div>Caricamento...</div>; // o uno spinner se vuoi
+  }
 
   return (
     
