903887f76db6a3a36712260691f6b1f34f848ec5
giobocca03 <gio.bocca03@gmail.com>
2025-07-09 15:42:28 +0200

    aggiunto rilevamento automatico posizione foto

--- frontend/src/pages/fisherdex/uploadFish/uploadFish.jsx
+++ frontend/src/pages/fisherdex/uploadFish/uploadFish.jsx
@@ -1,14 +1,44 @@
 import "./uploadFish.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function UploadFish({setShowUploadFish, specie} ) {
-  const [selectedImage, setSelectedImage] = useState(null);
 const [previewUrl, setPreviewUrl] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Form submitted");
   };
+
+  const recuperaPosizione= async()=>{
+    try{
+    
+    const posizioneUtente = await new Promise((resolve, reject)=>{
+      navigator.geolocation.getCurrentPosition(resolve, reject)
+    });
+
+    const latitudine = posizioneUtente.coords.latitude;
+    const longitudine = posizioneUtente.coords.longitude;
+    console.log(posizioneUtente.coords.accuracy)
+
+  
+    
+      const response = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${latitudine}&lon=${longitudine}&format=json`)
+      const data = await response.json();
+console.log(data.address.county);
+
+
+
+  }catch(error){
+    console.error("errore nel recupero della città", error)
+  }
+}
+  
+  useEffect(()=>{
+  recuperaPosizione();
+},[])
+  
+
+ 
 
   return (
     <div className="upload-fish-container">
@@ -26,32 +56,34 @@ const [previewUrl, setPreviewUrl] = useState(null);
               <div className="upload-fish-form-image">
         <label>Upload Fish</label>
 
-        
-      <input 
-  type="file" 
-  accept="image/*" 
-  capture="environment" 
-  onChange={(e) => {
-    const file = e.target.files[0];
-    if (file) {
-      setSelectedImage(file);
-      setPreviewUrl(URL.createObjectURL(file)); // genera l'anteprima
-      }
-    }}
-/>
-{previewUrl && (
-  <div className="preview-container">
-    <p>Anteprima:</p>
-    <img src={previewUrl} alt="Anteprima" className="preview-image" />
-  </div>
-)}
+        <div className="upload-fish-form-image-input">
+        <input 
+            type="file" 
+            accept="image/*" 
+            capture="environment" 
+            onChange={(e) => {
+              const file = e.target.files[0];
+              if (file) {
+                setPreviewUrl(URL.createObjectURL(file)); // genera l'anteprima
+                }
+              }} 
+        />
+        </div>
+
+        {previewUrl && (
+          <div className="preview-container">
+            <p>Anteprima:</p>
+            <img src={previewUrl} alt="Anteprima" className="preview-image" />
+          </div>
+        )}  
 
 
-<div className="button-section-form">
-<button type="submit">Upload</button>
-<button onClick={()=>setShowUploadFish(false)}>Close</button>
-</div>
-</div>
+
+        <div className="button-section-form">
+        <button type="submit">Upload</button>
+        <button onClick={()=>setShowUploadFish(false)}>Close</button>
+        </div>
+        </div>
  
 
       </form>
