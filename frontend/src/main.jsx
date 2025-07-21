a9f92ce5791b4e208bf5c9bd9de2bf4592fc6f1c
giobocca03 <gio.bocca03@gmail.com>
2025-07-21 14:09:41 +0200

    sistemato problema formntend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,9 +8,6 @@ import store from './app/store.js';
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <StrictMode>
-
-      <App />
-    </StrictMode>
+    <App />
   </Provider>
 )
